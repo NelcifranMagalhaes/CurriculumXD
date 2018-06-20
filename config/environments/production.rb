77fe9c1678c82b2e6b326b849352a41a95c9fd99
Nelcifran <nelcifranpires@gmail.com>
2018-06-20 13:24:54 -0300

    config of production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.precompile += ['pdf1.css.scss', 'pdf.css.scss']
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
