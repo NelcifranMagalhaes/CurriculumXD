73e5a54cda039231200d8488c89b12fb6643b05c
Nelcifran <nelcifranpires@gmail.com>
2018-07-09 12:54:07 -0300

    fix mailer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
 	
 	def questions
 		if params[:email].present? && params[:description].present?
-			UserMailer.email_questions(params[:email],params[:description])
+			UserMailer.email_questions(params[:email],params[:description]).deliver
 			#puts "#{params[:email]} e #{params[:description]} !!!!!!!!!!!!"
 		else
 			puts "Tudo nulo !!!!!!!!!!!!" 		
