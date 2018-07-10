1cb607b311b6569ee4362eaf80988101b2af961a
Nelcifran <nelcifranpires@gmail.com>
2018-07-10 09:31:00 -0300

    production edited

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "https://curriculumxd.herokuapp.com" }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
